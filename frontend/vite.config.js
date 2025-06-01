7b75e02e9d6c1868dc2ba58822b1994119cf8255
Johnny-Dang <dangleanhtuanpaula@gmail.com>
2025-06-01 15:36:46 +0700

    branch homepage

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    port: 3000, // ⚠️ đổi số port tùy ý (mặc định là 5173)
+    open: true, // (tuỳ chọn) tự động mở trình duyệt khi chạy
+  },
+});
