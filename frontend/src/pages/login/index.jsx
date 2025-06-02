decbec874933f7b53d597d567930db40ef4102b4
Johnny-Dang <dangleanhtuanpaula@gmail.com>
2025-06-02 11:10:27 +0700

    refactor: folder frontend

---
+++ frontend/src/pages/login/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import AuthenTemplate from '../../components/authen-template'
+
+function LoginPage() {
+  return (
+    <div>
+      <AuthenTemplate isLogin={true}></AuthenTemplate>
+    </div>
+  )
+}
+
+export default LoginPage
---
+++ frontend/src/pages/register/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import AuthenTemplate from '../../components/authen-template'
+
+function RegisterPage() {
+  return (
+    <div>
+      <AuthenTemplate isLogin={false}></AuthenTemplate>
+    </div>
+  )
+}
+
+export default RegisterPage
