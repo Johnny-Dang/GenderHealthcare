f19566c502011149aebf5c37f225e967e3cb9912
Johnny-Dang <dangleanhtuanpaula@gmail.com>
2025-06-03 09:06:39 +0700

    fix: eslint and prettier

--- frontend/src/components/authen-template/index.jsx
+++ frontend/src/components/authen-template/index.jsx
@@ -4,9 +4,9 @@ import LoginForm from '../authen-form/LoginForm'
 import RegisterForm from '../authen-form/RegisterForm'
 function AuthenTemplate({ isLogin }) {
   return (
-    <div className="authen-template">
-      <div className="authen-template__form">{isLogin ? <LoginForm /> : <RegisterForm />}</div>
-      <div className="authen-template__image"></div>
+    <div className='authen-template'>
+      <div className='authen-template__form'>{isLogin ? <LoginForm /> : <RegisterForm />}</div>
+      <div className='authen-template__image'></div>
     </div>
   )
 }
