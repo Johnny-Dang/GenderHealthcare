8b3ee11a1a7be919c5eb245f861f0c7fccea9280
Johnny-Dang <dangleanhtuanpaula@gmail.com>
2025-06-07 13:06:34 +0700

    new version

--- backend/Infrastructure/Extensions/AddDependencyInjection.cs
+++ backend/Infrastructure/Extensions/AddDependencyInjection.cs
@@ -2,7 +2,6 @@ using backend.Application.Common.Mappings;
 using backend.Application.Interfaces;
 using backend.Application.Services;
 using backend.Infrastructure.Persistence;
-using backend.Infrastructure.Services;
 using System.Net.NetworkInformation;
 using System.Runtime.CompilerServices;
 
@@ -14,10 +13,7 @@ namespace backend.Infrastructure.Extensions
         {
             serviceCollection.AddScoped<IRoleService, RoleService>();
             serviceCollection.AddScoped<IAccountService, AccountService>();
-<<<<<<< HEAD
-=======
             
->>>>>>> 292d8062ac651ef538500e6b65f204a94616d352
         }
     }
 }
