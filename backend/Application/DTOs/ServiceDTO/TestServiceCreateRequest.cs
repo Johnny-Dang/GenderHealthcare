838bd3bc54c67cb1668801040f4a6234343895b6
Nguyen Anh Tuan <nguyenanhtuandl1504@gmail.com>
2025-06-09 09:00:16 +0700

    git add configue and DTO for test service

---
+++ backend/Application/DTOs/ServiceDTO/TestServiceCreateRequest.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace backend.Application.DTOs.ServiceDTO
+{
+    public class TestServiceCreateRequest
+    {
+        public string ServiceName { get; set; } = default!;
+        public string Description { get; set; } = default!;
+        public decimal Price { get; set; }
+        public string? ImageUrl { get; set; }
+        public string Duration { get; set; } = default!;
+        public string Category { get; set; } = default!;
+
+    }
+}
